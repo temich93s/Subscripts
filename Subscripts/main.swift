f3e87cec10703f6ea7d170bcb145bdfb519b778e
Temich93s <nil>
2021-10-04 08:40:38 +1000

    Пройдена тема: Синтаксис сабскрипта

--- Subscripts/main.swift
+++ Subscripts/main.swift
@@ -9,3 +9,28 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Синтаксис сабскрипта
+print("\n//Синтаксис сабскрипта")
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+struct TimesTable1 {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        get {
+            return multiplier * index
+        }
+    }
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+print(threeTimesTable[3])
+
+let threeTimesTable1 = TimesTable1(multiplier: 4)
+print(threeTimesTable1[4])
